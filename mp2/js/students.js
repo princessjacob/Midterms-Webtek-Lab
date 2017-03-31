8eecd496b7ac6bba892d03d94944709c0d53309e
Elijah Pagayonan <pagayonan.eli@gmail.com>
2017-03-31 17:34:46 +0800

    Updated content of student.js

--- mp2/js/students.js
+++ mp2/js/students.js
@@ -1,40 +1,89 @@
-function getDetails(){
-    var req = new XMLHttpRequest();
-    var url = "http://localhost/html/sample.txt";
-    req.open("GET", url);
-    req.onreadystatechange = function() {
-        var ul = document.getElementById("pendingreq");
-        if (this.readyState == this.DONE && this.status == 200) {
-            if (this.responseText != null) {
-                var par = JSON.parse(this.responseText);
-               // saveToLocalStorage(par);
-                showRequests(this.responseText);
-            }
-            else {
-                ul.innerHTML = "no data found";
-            }
-        }    
-    };
-    req.send();
+function getDetails(){ 
+        var req = new XMLHttpRequest();
+        var url = "http://localhost/sample.txt";
+        req.open("GET", url);
+        req.onreadystatechange = function() {
+            var ul = document.getElementById("pendingreq");
+            if (this.readyState == this.DONE && this.status == 200) {
+                if (this.responseText != null) {
+                    var par = JSON.parse(this.responseText);
+                   // saveToLocalStorage(par);
+                    showRequests(this.responseText);
+                }
+                else {
+                    ul.innerHTML = "no data found";
+                }
+            }    
+        };
+        req.send();
+    
 }
 
+
+
  function showRequests(x){
+      var updated;
       var get = JSON.parse(x);
-     var testing = 0;
-     for(var i = 0; i <get.students.length; i++){
-         var temp = get.students[i];
-         if(document.getElementById("idNumber").value == temp.idnumber){
-//             document.getElementById("display").innerHTML= temp.name + " " + temp.section + " " + temp.age + " " + temp.parentsContact ; //
-             if(document.getElementById("late").checked == true){
-                testing++;
-                 document.getElementById("display").innerHTML = testing;
-             }
-                     
-         
-     }
+      var temp = -1;
+      var log = [];
+      if(localStorage.Processed) {
+        log = JSON.parse(localStorage.Processed);
+      }
+      var name = "";
      
+     for(var i = 0; i <get.length; i++){
+         
+                //   var testing = temp.vioLate;
+             if(document.getElementById("idNumber").value == get[i].idnumber){
+                document.getElementById("display1").innerHTML = get[i].idnumber + get[i].name;
+                name = get[i].name;
+                temp = i;  
+                break;  
+            }
+     
+    }
+
+    if(temp >= 0 ){
+
+             if(document.getElementById("late").checked == true){
+                 document.getElementById("display2").innerHTML = "Late:";     
+             }
+             if(document.getElementById("absent").checked == true){
+                 document.getElementById("display1").innerHTML = "Cut class:";
+             }
+             if(document.getElementById("noId").checked == true){
+                 document.getElementById("display1").innerHTML = "No Id:";
+             }
+             
+             if(document.getElementById("ImproperHairCut").checked == true){
+                 document.getElementById("display1").innerHTML = "ImproperHairCut:";
+             }
+               if(document.getElementById("CutClass").checked == true){
+                 document.getElementById("display1").innerHTML = "Cut class :";
+             }
+              if(document.getElementById("ImproUniform").checked == true){
+                 document.getElementById("display1").innerHTML = "ImproperHairCut:";
+             }
+          
+      }
+      else{
+        document.getElementById("display").innerHTML = "no user found";
+        document.getElementById("display1").innerHTML = "";
+        document.getElementById("display2").innerHTML = "";
+     }
+
+    var logObj = {"date":document.getElementById("date").value, "idnum":document.getElementById("idNumber").value, "name":name};
+
+    log.push(logObj);
+
+    localStorage.Processed = JSON.stringify(log);
+
+    document.getElementById("idNumber").value = "";
+    document.getElementById("date").value = "";
+    document.getElementById("late").checked = "";
+    temp = -1;
  }
- }
+
 
 
 
