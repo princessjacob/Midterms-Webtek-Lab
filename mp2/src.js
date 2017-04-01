c671a14ba8df093844387fc56e5a5289ec79a9da
Elijah Pagayonan <pagayonan.eli@gmail.com>
2017-04-02 02:58:55 +0800

    Enabled search() function to display multiple results

--- mp2/src.js
+++ mp2/src.js
@@ -1,25 +1,22 @@
 function search() {
     var logger = [];
     logger = JSON.parse(localStorage.Processed);
-    var table = document.getElementById("tbl");
+    var trow; 
 
     for(var i = 0; i < logger.length; i++) {
-    	//var container = [];
         if(document.getElementById("search").value == logger[i].idnum) {
-            //container[i] = JSON.stringify(logger[i]);
-            //document.getElementById("displaymo").innerHTML = container[i];
-            
-            var row = table.insertRow(i);
-            var cell1 = row.insertCell(0);
-            var cell2 = row.insertCell(1);
-            var cell3 = row.insertCell(2);
-            var cell4 = row.insertCell(3);
-            
-            cell1.innerHTML = logger[i].date,
-            cell2.innerHTML = logger[i].idnum,
-            cell3.innerHTML = logger[i].name,
-            cell4.innerHTML = logger[i].absent;
+            trow = document.createElement("tr");
+        	var tb = document.getElementById("tby");
+        	tb.appendChild(trow);
+
+			var cell1 = trow.insertCell(0);
+			var cell2 = trow.insertCell(1);
+			var cell3 = trow.insertCell(2);
+			var cell4 = trow.insertCell(3);
+			cell1.innerHTML = logger[i].date;
+			cell2.innerHTML = logger[i].idnum;
+			cell3.innerHTML = logger[i].name;
+			cell4.innerHTML = logger[i].late;
         }
-    }
-    
+    }  
 }
