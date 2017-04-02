d3492650ddc5bd3759e8676a07834ff4c2f82b9e
Elijah Pagayonan <pagayonan.eli@gmail.com>
2017-04-02 11:22:52 +0800

    Updated function to separate violations of student per row

--- mp2/src.js
+++ mp2/src.js
@@ -6,17 +6,21 @@ function search() {
     for(var i = 0; i < logger.length; i++) {
         if(document.getElementById("search").value == logger[i].idnum) {
             trow = document.createElement("tr");
-        	var tb = document.getElementById("tby");
-        	tb.appendChild(trow);
+       	    var tb = document.getElementById("tby");
+            tb.appendChild(trow);
 
-			var cell1 = trow.insertCell(0);
-			var cell2 = trow.insertCell(1);
-			var cell3 = trow.insertCell(2);
-			var cell4 = trow.insertCell(3);
-			cell1.innerHTML = logger[i].date;
-			cell2.innerHTML = logger[i].idnum;
-			cell3.innerHTML = logger[i].name;
-			cell4.innerHTML = logger[i].late;
+	    var cell1 = trow.insertCell(0);
+	    var cell2 = trow.insertCell(1);
+	    var cell3 = trow.insertCell(2);
+	    var cell4 = trow.insertCell(3);
+	    cell1.innerHTML = logger[i].date;
+	    cell2.innerHTML = logger[i].idnum;
+	    cell3.innerHTML = logger[i].name;
+	    if (logger[i].late) {
+                cell4.innerHTML = logger[i].late;
+            } else if (logger[i].absent) {
+                cell4.innerHTML = logger[i].absent;
+            }
         }
     }  
 }
