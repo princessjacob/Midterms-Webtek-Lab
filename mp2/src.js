607df6a3a608ce51a5cb1f943a35453c3be0861a
Ma. Danavie <dnvsayco@gmail.com>
2017-04-02 11:54:42 +0800

    Removed previous results upon making a new search

--- mp2/src.js
+++ mp2/src.js
@@ -1,38 +1,26 @@
 function search() {
     var logger = [];
     logger = JSON.parse(localStorage.Processed);
+    var tb = document.getElementById("tby");
+    tb.innerHTML = '';
     var trow; 
 
     for(var i = 0; i < logger.length; i++) {
         if(document.getElementById("search").value == logger[i].idnum) {
             trow = document.createElement("tr");
-       	    var tb = document.getElementById("tby");
             tb.appendChild(trow);
-
-	    var cell1 = trow.insertCell(0);
-	    var cell2 = trow.insertCell(1);
-	    var cell3 = trow.insertCell(2);
-	    var cell4 = trow.insertCell(3);
-	    cell1.innerHTML = logger[i].date;
-	    cell2.innerHTML = logger[i].idnum;
-	    cell3.innerHTML = logger[i].name;
-	    if (logger[i].late) {
+	       var cell1 = trow.insertCell(0);
+	       var cell2 = trow.insertCell(1);
+	       var cell3 = trow.insertCell(2);
+	       var cell4 = trow.insertCell(3);
+	       cell1.innerHTML = logger[i].date;
+	       cell2.innerHTML = logger[i].idnum;
+	       cell3.innerHTML = logger[i].name;
+	       if (logger[i].late) {
                 cell4.innerHTML = logger[i].late;
-            } else if (logger[i].absent) {
+           } else if (logger[i].absent) {
                 cell4.innerHTML = logger[i].absent;
-            } else if (logger[i].noID) {
-                cell4.innerHTML = logger[i].noID;
-            } else if (logger[i].imporperHairCut) {
-                cell4.innerHTML = logger[i].imporperHairCut;
-            } else if (logger[i].CutClass){
-                cell4.innerHTML = logger[i].CutClass;
-            } else if (logger[i].improperUniform) {
-                cell4.innerHTML = logger[i].improperUniform;
-            } else if (logger[i].gadget) {
-                cell4.innerHTML = logger[i].gadget;
-            } else if (logger[i].violence) {
-                cell4.innerHTML = logger[i].violence;
-            }
+           }
         }
     }  
 }
